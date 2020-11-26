25a93f4ea5a0c8af88e738c00efbc9a47745345c
Melanie Gravelle <melanie.gravelle@hotmail.fr>
2020-11-26 13:45:00 +0000

    add line on bitbucket

--- test.php
+++ test.php
@@ -2,10 +2,12 @@
 
 echo 'Ceci est mon premier fichier php test';
 
-echo 'J Ã©cris ma seconde phrase';
+echo 'J Žcris ma seconde phrase';
 
 echo 'Pour sauvegarder il faut appuyer sur ESC puis : puis W puis Q puis !';
 
 echo 'nous testons l envoit sur bitbucket';
+
+echo 'Je viens de bitbucket';
 
 ?>
